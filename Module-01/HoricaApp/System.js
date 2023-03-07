dea0f2d657ce8770c034c536c9db54151e335b98
Rexouium <mitch.brederije@gmail.com>
2023-03-07 13:18:11 +0100

    Push

--- Module-01/HoricaApp/System.js
+++ Module-01/HoricaApp/System.js
@@ -7,57 +7,41 @@ totaal = 0.0
 FrisTotal = 0.0
 BierTotal = 0.0
 WijnTotal = 0.0
-//Prices
-FrisPrijs = 0.5
-BierPrijs = 1
-WijnPrijs = 1.5
 //vars
 let OrderMaking = true;
 let UserInputs = '';
 let AantalDrinks = 0;
-
+//dictionaries
+dictionary = {}
+products = {'Fris': 0.5, 'Bier': 1, 'Wijn': 1.5}
 //start of system
 
 while (OrderMaking) {
     UserInputs = prompt("Wat wilt u bestellen? Keuze uit Fris, Bier of Wijn, Wilt u stoppen? type Stop: ")
-
-    if (UserInputs !== "Fris" && UserInputs !== "Bier" && UserInputs !== "Wijn" && UserInputs !== "Stop") {
-        alert("Invalid Argument(s) given!")
+    if (UserInputs === "Stop") {
+        OrderMaking = false
     }
     else {
-        if (UserInputs === "Stop") {
-            OrderMaking = false
+        if (UserInputs !== "Fris" && UserInputs !== "Bier" && UserInputs !== "Wijn") {
+            alert("Invalid Argument(s) given!")
         }
-        else {
-            AantalDrinks = parseInt(prompt(`Hoe veel ${UserInputs} wilt u hebben?: `))
-        }
+        AantalDrinks = parseInt(prompt(`Hoe veel ${UserInputs} wilt u hebben?: `))
+        totaal += AantalDrinks * products[UserInputs]
+        Object.keys(dictionary).includes(UserInputs) ? dictionary[UserInputs] += AantalDrinks : dictionary[UserInputs] = AantalDrinks
+        console.log(totaal)
     }
-
-    if (UserInputs === "Fris") {
-        Fris += AantalDrinks
-    }
-    if (UserInputs === "Bier") {
-        Bier += AantalDrinks
-    }
-    if (UserInputs === "Wijn") {
-        Wijn += AantalDrinks
-    }
+    dictionary[UserInputs] += AantalDrinks
 //  ||Out off first while||
 }// ||                   ||
 //  ||Out off first while||
 
 
-//Calculate part start
-FrisTotal = Fris * FrisPrijs
-BierTotal = Bier * BierPrijs
-WijnTotal = Wijn * WijnPrijs
-totaal = (FrisTotal + BierTotal + WijnTotal)
-//Calculate part end
+for (const [key, value] of Object.entries(dictionary)) { // replace dictionary
+    let display = `${value}x ${key} - €${(value * products[key]).toFixed(2).replace(".", ",")}`
+    document.getElementById("bon").innerText = "Uw bon - bedankt voor het bestellen!"
+    document.getElementById("bon-producten").innerText += display + '\n';
 
-alert(`
-    Hier is je bonnetje:
-    ${Fris} aantal fris, dit komt uit op: ${FrisTotal}
-    ${Bier} aantal bier, dit komt uit op: ${BierTotal}
-    ${Wijn} aantal wijn, dit komt uit op: ${WijnTotal}
-    Je totaal bedraagt: ${totaal}`
-)
+    if (totaal > 0) {
+        document.getElementById("totaalprijs").innerText = "totaal: €" + totaal.toFixed(2).replace(".", ",")
+    }
+}
