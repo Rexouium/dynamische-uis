7c534471e5d0a243efa01fe9fdba95f7f8c1b80e
Rexouium <mitch.brederije@gmail.com>
2023-03-07 13:53:03 +0100

    Updated

--- Module-01/HoricaApp/System.js
+++ Module-01/HoricaApp/System.js
@@ -1,47 +1,46 @@
-//tags
-Fris = 0
-Bier = 0
-Wijn = 0
-//totals for calculating
-totaal = 0.0
-FrisTotal = 0.0
-BierTotal = 0.0
-WijnTotal = 0.0
-//vars
-let OrderMaking = true;
-let UserInputs = '';
-let AantalDrinks = 0;
-//dictionaries
-dictionary = {}
-products = {'Fris': 0.5, 'Bier': 1, 'Wijn': 1.5}
-//start of system
+totaal = 0;
+receipt = {}
+products = {'fris': 0.50, 'bier': 1.00, 'wijn': 1.50}
 
-while (OrderMaking) {
-    UserInputs = prompt("Wat wilt u bestellen? Keuze uit Fris, Bier of Wijn, Wilt u stoppen? type Stop: ")
-    if (UserInputs === "Stop") {
-        OrderMaking = false
-    }
-    else {
-        if (UserInputs !== "Fris" && UserInputs !== "Bier" && UserInputs !== "Wijn") {
-            alert("Invalid Argument(s) given!")
+let ordering = true;
+
+function getNum(text) {
+    while (true) {
+        let retrieveNum = parseInt(prompt(text))
+
+        if (!isNaN(retrieveNum)) {
+            return retrieveNum
+        } else {
+            alert("We hebben een getal nodig.")
         }
-        AantalDrinks = parseInt(prompt(`Hoe veel ${UserInputs} wilt u hebben?: `))
-        totaal += AantalDrinks * products[UserInputs]
-        Object.keys(dictionary).includes(UserInputs) ? dictionary[UserInputs] += AantalDrinks : dictionary[UserInputs] = AantalDrinks
-        console.log(totaal)
     }
-    dictionary[UserInputs] += AantalDrinks
-//  ||Out off first while||
-}// ||                   ||
-//  ||Out off first while||
+}
 
+while (ordering) {
+    userInput = prompt(`Welk product? U kunt kiezen tussen ${Object.keys(products)}`).toLowerCase()
 
-for (const [key, value] of Object.entries(dictionary)) { // replace dictionary
-    let display = `${value}x ${key} - €${(value * products[key]).toFixed(2).replace(".", ",")}`
+    if (userInput === "stop") {
+        ordering = false;
+        console.log("stopping...")
+    } else {
+
+        if (!(userInput in products)) {
+            alert("Wij kunnen dit product niet aanbieden. U hebt de keuze uit " + Object.keys(products))
+        } else {
+            productAmount = getNum(`Hoeveel wilt u van ${userInput}?: `)
+
+            userInput in receipt ? receipt[userInput] += productAmount : receipt[userInput] = productAmount
+
+            totaal += productAmount * products[userInput]
+        }
+    }
+}
+
+for (const [key, value] of Object.entries(receipt)) {
+    let display = `${value}x ${key} - €${(value*products[key]).toFixed(2).replace(".", ",")}`
     document.getElementById("bon").innerText = "Uw bon - bedankt voor het bestellen!"
     document.getElementById("bon-producten").innerText += display + '\n';
-
-    if (totaal > 0) {
-        document.getElementById("totaalprijs").innerText = "totaal: €" + totaal.toFixed(2).replace(".", ",")
-    }
+}
+if (totaal > 0) {
+    document.getElementById("totaalprijs").innerText = "totaal: €" + totaal.toFixed(2).replace(".", ",")
 }
