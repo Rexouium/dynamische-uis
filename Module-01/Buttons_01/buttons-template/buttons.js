311da0af2d6274f7b18d15998722bcab951cfc05
Rexouium <mitch.brederije@gmail.com>
2023-03-23 14:08:55 +0100

    Adding the buttons 01 files again

--- Module-01/Buttons_01/buttons-template/buttons.js
+++ Module-01/Buttons_01/buttons-template/buttons.js
@@ -1,15 +1,12 @@
-
-
-function button_event_handler(IdVar){
-    print()
+ function button_event_handler0(IdVar){
+        document.getElementById("backgroundimg0").style.backgroundImage = `url('images/bg${IdVar}.jpg')`;
 }
-
+    function button_event_handler1(IdVar){
+        document.getElementById("backgroundimg1").style.backgroundImage = `url('images/bg${IdVar}.jpg')`;
+}
 function button_event_handler2(IdVar){
-    print()
+        document.getElementById("backgroundimg2").style.backgroundImage = `url('images/bg${IdVar}.jpg')`;
 }
-
-function button_event_handler3(IdVar){
-    print()
+    function button_event_handler3(IdVar){
+        document.getElementById("backgroundimg3").style.backgroundImage = `url('images/bg${IdVar}.jpg')`;
 }
-placeholder = ["test"]
-button_event_handler(placeholder)
