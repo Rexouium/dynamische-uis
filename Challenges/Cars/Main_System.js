85f3a4ff984b49399384ae97f51a6baa45fb210b
Rexouium <mitch.brederije@gmail.com>
2023-04-11 10:04:42 +0200

    Finishing Buttons2

---
+++ Challenges/Cars/Main_System.js
@@ -0,0 +1,5 @@
+Car_Name_List = cars
+
+Ferrari_Finder = Car_Name_List.find("Ferrari")
+
+text
--- Module-01/Buttons_01/buttons-template/buttons.js
+++ Module-01/Buttons_01/buttons-template/buttons.js
@@ -1,6 +1,21 @@
+IdVar = 5
 function button_event_handler(IdVar) {
     console.log(IdVar)
     document.getElementById("backgroundimg0").src = `images/bg${IdVar}.jpg`;
     document.getElementById("containerimg0").src = `images/${IdVar}.jpg`
     document.getElementById(`Button${IdVar}`).innerText = parseInt(document.getElementById(`Button${IdVar}`).innerText) + 1
+    pressedlast = document.getElementById(`Button${IdVar}`).disabled = true
+    pressedlast = document.getElementById(`Button${IdVar}`).style.backgroundColor = "red"
+    lastpressedid = document.getElementById(`button`)
+    button_handeler(IdVar)
+}
+function button_handeler(IdVar){
+    totalbuttons = 4
+    for (let i = 0; i < totalbuttons; i++) {
+        document.getElementById(`Button${i}`).style.backgroundColor = "green"
+        document.getElementById(`Button${i}`).disabled = false
+    }
+
+    pressedlast = document.getElementById(`Button${IdVar}`).disabled = true
+    pressedlast = document.getElementById(`Button${IdVar}`).style.backgroundColor = "red"
 }
